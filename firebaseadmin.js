a4653aa16914ff9eba95881b1093e45d4abef2c4
neavyartical <neavyartical@gmail.com>
2026-04-14 18:38:14 +0100

    firebaseadmin.js

--- firebaseadmin.js
+++ firebaseadmin.js
@@ -1,9 +1,9 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
+const firebaseConfig = {
+  apiKey: "AIzaSyCraJ2lDNrPU7O_qDz4J6_aCNrR_l4ex-I",
+  authDomain: "reelmind-ai-a11eb.firebaseapp.com",
+  projectId: "reelmind-ai-a11eb",
+  storageBucket: "reelmind-ai-a11eb.firebasestorage.app",
+  messagingSenderId: "534455151705",
+  appId: "1:534455151705:web:42eef09cd465fecb8ed548",
+  measurementId: "G-NSD0WSPC5D"
+};
