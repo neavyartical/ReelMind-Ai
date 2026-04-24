c1a2f1d16d6f65aed16b5017a74fc713317bfc77
neavyartical <neavyartical@gmail.com>
2026-04-24 05:23:53 +0100

    socket.js

--- socket.js
+++ socket.js
@@ -1,1 +1,57 @@
+// socket.js
+import { auth } from "./firebase.js";
+import { API } from "./script.js";
 
+import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
+
+/* =========================
+   SOCKET CONNECTION
+========================= */
+export const socket = io(API, {
+  transports: ["websocket"],
+  autoConnect: false
+});
+
+/* =========================
+   CONNECT USER
+========================= */
+function registerUser(user) {
+  if (!user?.uid) return;
+
+  if (!socket.connected) {
+    socket.connect();
+  }
+
+  socket.emit("register", user.uid);
+}
+
+/* =========================
+   DISCONNECT USER
+========================= */
+function unregisterUser() {
+  if (socket.connected) {
+    socket.disconnect();
+  }
+}
+
+/* =========================
+   AUTH WATCHER
+========================= */
+auth.onAuthStateChanged(user => {
+  if (user) {
+    registerUser(user);
+  } else {
+    unregisterUser();
+  }
+});
+
+/* =========================
+   DEBUG
+========================= */
+socket.on("connect", () => {
+  console.log("Socket connected:", socket.id);
+});
+
+socket.on("disconnect", () => {
+  console.log("Socket disconnected");
+});
